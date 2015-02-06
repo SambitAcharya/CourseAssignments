1a54566e98527309a6f751a0b32124105c31732c
SambitAcharya <sambitacharya121@gmail.com>
2015-02-07 02:48:26 +0530

    Added file for problem 3.

---
+++ Data Structures And Algorithms/Assignment 1/problem3.cpp
@@ -0,0 +1,17 @@
+/**
+
+  Write a program to find the number of times that a given word(i.e. a short string) occurs in a sentence (i.e. a long string!). Read data from standard input. The first line is a single word, which is followed by general text on the second line. Read both up to a newline character, and insert a terminating null before processing.
+  Typical output should be:
+    The word is "the".
+    The sentence is "the cat sat on the mat".
+    “the” occured 2 times.
+
+*/
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	return 0;
+}
