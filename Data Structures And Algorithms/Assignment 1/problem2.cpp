13e2c294fc753199880348ebcea5ea322660550e
SambitAcharya <sambitacharya121@gmail.com>
2015-02-06 19:14:42 +0530

    Added problem statement 2.

---
+++ Data Structures And Algorithms/Assignment 1/problem2.cpp
@@ -0,0 +1,20 @@
+/**
+
+Write a program for implementing each of the following functions (don’t use library string functions):
+a. Compute the length of a string using pointers (int strlen(char *str))
+b. Copy a string from one location to another (strcpy(char *src, char *dest))
+c. Concatenate two strings ‘s’ and ‘t’ (char *strcat(char *s, char *t))
+d. Returns 1 if the string t occurs at the end of string s, and otherwise returns 0
+(int strend(char *s, char *t))
+e. Print if the given string is a palindrome or not (void checkPalindrome(char *s))
+f. Reverse a given string (char *reverse(char *original))
+
+*/
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	return 0;
+}
