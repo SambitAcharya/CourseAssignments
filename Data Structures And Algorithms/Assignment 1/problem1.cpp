f90f315c12a2dd069d83d09b53922703f7c05578
SambitAcharya <sambitacharya121@gmail.com>
2015-02-06 18:18:11 +0530

    Added problem statement.

---
+++ Data Structures And Algorithms/Assignment 1/problem1.cpp
@@ -0,0 +1,15 @@
+/**
+
+Write a function that takes an array of ints and its length as the arguments and
+uses pointers to initialize the elements of the array to zero.
+Return the array using a pointer. The function would be:  int *zero_array(int *a, int length)
+
+*/
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	return 0;
+}
